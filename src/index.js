3dca5352b5192879a7c5b5cb7ce33d1c9f00207a
Javier Veron <javier.veron@gmail.com>
2023-08-05 12:09:18 -0300

    Fix de index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import {Provider} from "react-redux";
-import {createStore} from "redux";
-import store from "./redux/ProductosReducer";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Provider store={store}>
-<App />
-</Provider>);
+root.render(<App />);
